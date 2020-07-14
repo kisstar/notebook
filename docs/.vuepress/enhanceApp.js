59d943b9df8d294ba6fe16c7e308376c8a1fce3b
kisstar <dwh.chn@foxmail.com>
2020-07-14 17:22:05 +0800

    chore: fix ESLint error -> No files matching the pattern "." were found
    See https://stackoverflow.com/questions/54543063/

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,8 +1,10 @@
-export default ({
-  Vue, // 当前 VuePress 应用所使用的 Vue 版本
-  options, // 根 Vue 实例的选项
-  router, // 应用程序的路由实例
-  siteData, // 网站元数据
-}) => {
-  // ...使用应用程序级别的增强功能
-}
+export default () =>
+  // {
+  // Vue, // 当前 VuePress 应用所使用的 Vue 版本
+  // options, // 根 Vue 实例的选项
+  // router, // 应用程序的路由实例
+  // siteData, // 网站元数据
+  // },
+  {
+    // ...使用应用程序级别的增强功能
+  }
