60a9ba34eeb60ea8270b04c24f0118ff627f7ba2
Kisstar <dwh.chn@foxmail.com>
2024-03-03 13:59:56 +0800

    docs(algorithm): 设计循环双端队列

--- docs/.vuepress/nav/zh/algorithm.js
+++ docs/.vuepress/nav/zh/algorithm.js
@@ -20,7 +20,7 @@ module.exports = [
   {
     title: '队列',
     sidebarDepth: 2,
-    children: ['queue/', 'queue/232', 'queue/239'],
+    children: ['queue/', 'queue/232', 'queue/239', 'queue/641'],
   },
   {
     title: '链表',
