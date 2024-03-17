54ab7ce260c935de43da430bfe26e8a0eb69da0e
Kisstar <dwh.chn@foxmail.com>
2024-03-17 13:39:20 +0800

    docs(algorithm): 按 K 个一组翻转链表

--- docs/.vuepress/nav/zh/algorithm.js
+++ docs/.vuepress/nav/zh/algorithm.js
@@ -20,7 +20,7 @@ module.exports = [
   {
     title: '队列',
     sidebarDepth: 2,
-    children: ['queue/', 'queue/232', 'queue/239', 'queue/641', 'queue/203'],
+    children: ['queue/', 'queue/232', 'queue/239', 'queue/641', 'queue/203', 'queue/25'],
   },
   {
     title: '链表',
